5126040412035d3204b8894e831d3874fc1d4549
Brittany Morris <brittmmendez@gmail.com>
2018-01-17 16:55:04 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,11 +3,13 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
+    #could also do "#{self.first_name} #{self.last_name}"
     self.first_name + " " + self.last_name
   end
 
   def list_roles
     self.characters.collect do |character|
+      #could also do "#{character.name} - #{character.show.name}"
     character.name + " - " + character.show.name
     end
   end

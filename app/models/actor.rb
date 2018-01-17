f5283e4027aee0f4ed501525d2543875fb1d856c
Brittany Morris <brittmmendez@gmail.com>
2018-01-17 16:38:18 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,9 +9,9 @@ class Actor < ActiveRecord::Base
   def list_roles
     self.characters.collect do |character|
       character.name
-      #characters.show.name
+      character.show.name
   end
-  binding.pry
+  
   end
 
 end
